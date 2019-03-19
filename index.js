11e8520d5997a4145381521131dabc78253c6a88
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-03-19 10:41:26 +0100

    refactor: launch http request asynchronously

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const send = require("@polka/send-type");
 // CONSTANTS
 const VIEW_DIR = join(__dirname, "views");
 const LINK_FILE = join(__dirname, "data", "link.json");
+
+// Globals
 const token = process.env.GIT_TOKEN;
+const projectLink = Object.create(null);
+const orphans = new Set();
 
 async function startHTTPServer(data = {}) {
     console.log(green("Starting HTTP Server on port: 1337"));
@@ -24,6 +28,38 @@ async function startHTTPServer(data = {}) {
         .get("/", (req, res) => send(res, 200, view, { "Content-Type": "text/html" }))
         .get("/data", (req, res) => send(res, 200, data))
         .listen(1337);
+}
+
+async function processPackage(repo, URL) {
+    console.log(`- Processing ${yellow(repo.fullName)}`);
+    try {
+        const { data } = await get(URL, {
+            headers: {
+                "User-Agent": "SlimIO",
+                Authorization: `token ${token}`,
+                Accept: "application/vnd.github.v3.raw"
+            }
+        });
+
+        const pkg = JSON.parse(data);
+        const filteredDeps = Object.keys(pkg.dependencies || {}).filter((name) => name.startsWith("@slimio"));
+
+        for (const dep of filteredDeps) {
+            const [, name] = dep.split("/");
+            const fName = name.toLowerCase();
+
+            projectLink[repo.name].dependOn.push(fName);
+            if (Reflect.has(projectLink, fName)) {
+                projectLink[fName].uses.push(repo.name);
+            }
+            else {
+                orphans.add(fName);
+            }
+        }
+    }
+    catch (err) {
+        console.log(red(`Failed to retrieve project: ${yellow(repo.fullName)}`));
+    }
 }
 
 async function main() {
@@ -36,64 +72,35 @@ async function main() {
     }
 
     console.time("gen_link");
-    const orphans = new Set();
-    const projectLink = Object.create(null);
-
     const fullRepositories = await repos("SlimIO", { token });
     const filteredRepositories = fullRepositories
         .filter((row) => row.archived === false)
         .map((row) => {
             const name = row.name.toLowerCase();
+            const license = row.license || {};
+
             projectLink[name] = Object.freeze({
-                uses: [], dependOn: []
+                size: row.size || 0,
+                license: license.name || "N/A",
+                description: row.description || "",
+                uses: [],
+                dependOn: []
             });
 
             return {
                 id: row.id,
                 fullName: row.full_name,
                 name,
-                url: row.html_url,
-                desc: row.description,
-                // size: row.size,
-                // license: row.license,
                 defaultBranch: row.default_branch
             };
         });
 
-    // TODO: do this Asynchronously
+    const promises = [];
     for (const repo of filteredRepositories) {
-        console.log(`- Processing ${yellow(repo.fullName)}`);
         const packageURL = new URL(`https://api.github.com/repos/${repo.fullName}/contents/package.json`);
-
-        try {
-            const { data } = await get(packageURL, {
-                headers: {
-                    "User-Agent": "SlimIO",
-                    Authorization: `token ${token}`,
-                    Accept: "application/vnd.github.v3.raw"
-                }
-            });
-
-            const pkg = JSON.parse(data);
-            const slimioDep = Object.keys(pkg.dependencies || {}).filter((name) => name.startsWith("@slimio"));
-
-            for (const dep of slimioDep) {
-                const [, name] = dep.split("/");
-                const fName = name.toLowerCase();
-
-                projectLink[repo.name].uses.push(fName);
-                if (Reflect.has(projectLink, fName)) {
-                    projectLink[fName].dependOn.push(repo.name);
-                }
-                else {
-                    orphans.add(fName);
-                }
-            }
-        }
-        catch (err) {
-            console.log(red(`Failed to retrieve project: ${yellow(repo.fullName)}`));
-        }
+        promises.push(processPackage(repo, packageURL));
     }
+    await Promise.all(promises);
     console.timeEnd("gen_link");
 
     console.log("\nOrphans:");
